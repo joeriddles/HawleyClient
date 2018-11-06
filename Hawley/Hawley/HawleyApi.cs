ca4eb6eac84baca7138d68471f7fcb974aeb082e
joeriddles <joeriddles10@gmail.com>
2018-11-05 16:17:05 -0800

    Initial commit

---
+++ Hawley/Hawley/HawleyApi.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace Hawley
+{
+	class HawleyApi
+	{
+		static void Main()
+		{
+			var products = GetProductVariants("https://api.hawleyusa.com/v2.0/");
+			WriteProductVariantsToCsv(products);
+		}
+
+		private static List<ProductVariant> GetProductVariants(string url)
+		{
+			var client = new RestClient(url);
+			var request = new RestRequest("Catalog/Products/Variants", Method.GET);
+
+			request.AddHeader("Accept", "application/json");
+			request.AddHeader("Authorization", "ApiKey KEYGOESHERE");
+
+			request.AddParameter("pageStartIndex", "1");
+			request.AddParameter("pageSize", "5");
+
+			IRestResponse response = client.Execute(request);
+
+			var jArray = JsonConvert.DeserializeObject <JArray>(response.Content);
+			var productVariants = jArray.ToObject<List<ProductVariant>>();
+			return productVariants;
+		}
+
+		private static void WriteProductVariantsToCsv(List<ProductVariant> productVariants)
+		{
+			var csv = new StringBuilder();
+
+			csv.AppendLine("Handle,Title,Body(HTML)");
+
+			foreach (var pv in productVariants)
+			{
+				string title = string.Join(" ", pv.Attributes.SelectMany(
+					a => a.Attributes
+						.Select(
+							at => string.Join(" ", new List<string> {at.Name, at.Value})
+					)
+				));
+
+				var pvLine = $"{pv.VariantNo}," +
+				             $"{title}," +
+				             $"{pv.Description.Replace(",", " ")}";
+				csv.AppendLine(pvLine);
+			}
+
+			File.WriteAllText("..\\..\\..\\..\\HawleyProductVariants.csv", csv.ToString());
+		}
+	}
+}
---
+++ Hawley/Hawley/Product.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+
+namespace Hawley
+{
+	// returned from /Catalog/Products...
+	class Product
+	{
+		public string ProductId { get; set; }
+		public string ProductNo { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string BrandId { get; set; }
+		public string Brand { get; set; }
+		public string DateCreated { get; set; }
+		public string DateLastModified { get; set; }
+		public List<string> VariantIds { get; set; }
+		public List<string> CategoryIds { get; set; }
+	}
+
+	// returned from /Catalog/Products/Variants...
+	class ProductVariant
+	{
+		public string VariantId { get; set; }
+		public string VariantNo { get; set; }
+		public string ProductId { get; set; }
+		public string ProductNo { get; set; }
+		public string Description { get; set; }
+		public string StatusId { get; set; }
+		public string StatusDesc { get; set; }
+		public string Upc { get; set; }
+		public string Ean { get; set; }
+		public string Config { get; set; }
+		public string Size { get; set; }
+		public string Color { get; set; }
+		public string ProductDimensionGroup { get; set; }
+		public string MfgPartNumber { get; set; }
+		public bool GroundOnly { get; set; }
+		public string DateCreated { get; set; }
+		public string DateLastModified { get; set; }
+		public List<VariantAttribute> Attributes { get; set; }
+		public List<VariantPrice> Prices { get; set; }
+		public List<VariantImage> Images { get; set; }
+
+		public class VariantAttribute
+		{
+			public string VariantId { get; set; }
+			public string ProductId { get; set; }
+			public List<VariantAttributeInternal> Attributes { get; set; }
+
+			public class VariantAttributeInternal
+			{
+				public string Name { get; set; }
+				public string Value { get; set; }
+			}
+		}
+
+		public class VariantPrice
+		{
+			public string Price { get; set; }
+			public string PriceTypeId { get; set; }
+			public string PriceTypeDesc { get; set; }
+			public string QtyBreak { get; set; }
+		}
+
+		public class VariantImage
+		{
+			public string FormatId { get; set; }
+			public string Format { get; set; }
+			public string Url { get; set; }
+		}
+	}
+}
